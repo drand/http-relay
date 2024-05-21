b11e79a3ba894b88143eb2bb1c6522512f1db4ed
Yolan Romailler <anomalroil@users.noreply.github.com>
2024-05-21 10:05:50 +0200

    Using same flags as previous relay

--- main.go
+++ main.go
@@ -25,12 +25,15 @@ import (
 var (
 	version     = "drand-http-server-v2.0.1"
 	metricFlag  = flag.String("metrics", "localhost:9999", "The flag to set the interface for metrics. Defaults to localhost:9999")
-	grpcURL     = flag.String("grpc", "localhost:4444", "The URL and port to your drand node's grpc port, e.g. pl1-rpc.testnet.drand.sh:443 you can add fallback nodes by separating them with a comma: pl1-rpc.testnet.drand.sh:443,pl2-rpc.testnet.drand.sh:443")
+	httpBind    = flag.String("bind", "localhost:8080", "The address to bind the http server to")
+	grpcURL     = flag.String("grpc-connect", "localhost:4444", "The URL and port to your drand node's grpc port, e.g. pl1-rpc.testnet.drand.sh:443 you can add fallback nodes by separating them with a comma: pl1-rpc.testnet.drand.sh:443,pl2-rpc.testnet.drand.sh:443")
 	goVersion   = flag.Bool("version", false, "Displays the current server version.")
 	requireAuth = flag.Bool("enable-auth", false, "Forces JWT authentication on V2 API using the JWT secret from the AUTH_TOKEN env variable.")
 	verbose     = flag.Bool("verbose", false, "Prints as many logs as possible.")
 	jsonFlag    = flag.Bool("json", false, "Prints logs in JSON format.")
 	frontrun    = flag.Int64("frontrun", 0, "When waiting for the next round, start the query this amount of ms earlier to counteract network latency.")
+	_           = flag.String("insecure", "", "deprecated flag")
+	_           = flag.String("hash-list", "", "deprecated flag")
 )
 
 func init() {
@@ -65,7 +68,7 @@ func main() {
 	slog.Info("Starting http relay", "version", version, "client", client)
 
 	// The HTTP Server
-	server := &http.Server{Addr: "0.0.0.0:8080", Handler: setup(client)}
+	server := &http.Server{Addr: *httpBind, Handler: setup(client)}
 
 	// Server run context
 	serverCtx, serverStopCtx := context.WithCancel(context.Background())
